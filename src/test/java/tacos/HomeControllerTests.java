e0a4a9372752f0377d034f958b85181610f0eb4d
Lora <loragayle021@gmail.com>
2024-12-03 01:20:20 -0500

    Chapter two

---
+++ src/test/java/tacos/HomeControllerTests.java
@@ -0,0 +1,29 @@
+package tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import tacos.api.HomeController;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testHomePage() throws Exception {
+        ResultActions resultActions = mockMvc.perform(get("/"));
+        resultActions.andExpect(status().isOk());
+        resultActions.andExpect(view().name("home"));
+//        resultActions.andExpect(content().string(
+//                containsString("Welcome to The Taco Stand")));
+    }
+
+}
+
